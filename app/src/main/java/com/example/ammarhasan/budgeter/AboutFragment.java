d4929511e74a77717584cb30c0e2a3596c3797ac
Ammar Hasan <ammar199733@gmail.com>
2018-04-22 10:27:46 +0100

    Basic navigation and theme completed

---
+++ app/src/main/java/com/example/ammarhasan/budgeter/AboutFragment.java
@@ -0,0 +1,33 @@
+package com.example.ammarhasan.budgeter;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * @author Ammar Hasan 150454388 April 2018
+ * Class Purpose: This class contains the Home page
+ * fragment of the navigation activity funtionality
+ */
+public class AboutFragment extends Fragment {
+
+    @Nullable
+    @Override   // connects to layout file
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return  inflater.inflate(R.layout.fragment_about, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // use to do activity stuff, use view to find stuff and use getActivity to get context
+    }
+}
--- app/src/main/java/com/example/ammarhasan/budgeter/LoginActivity.java
+++ app/src/main/java/com/example/ammarhasan/budgeter/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
